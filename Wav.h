45181da954bc7bd5640af05c5bb5699265fdda5f
Tanner Richnak <trichnak@nevada.unr.edu>
2021-05-03 15:02:18 -0700

    5/3

--- Wav.h
+++ Wav.h
@@ -28,32 +28,14 @@ public:
 	std::string inFile;
 public:
 	Wav(std::string inFile);
-	/**
-	 * Function to read data from an inputted .wav file
-	 * @param filename - the name of the .wav file being read from; overrides from IReadable
-	 */
+	
     void readFile(const std::string &filename) override;
-    /**
-	 * Function to write data to a wav file
-	 * @param outFileName - the name of the .wav file being written to
-	 */
     void writeFile(const std::string &outFileName);   
-    
-    /**
-	 * Determines if the wav file has a List subchunk
-	 */
+
     bool checkForList();
-    /**
-	 * Returns the size of the data buffer in bytes
-	 */
     int getBufferSize() const;
-    /**
-	 * Returns a pointer to the data buffer
-	 */
     T *getBuffer() const;
-    /**
-	 * Returns the vector of List objects
-	 */
+
     std::vector<List> getListVector() const;
     
     /**
